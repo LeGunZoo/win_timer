a8c68a42c6270906710f42f32d12c45571f36a29
LeGunZoo <legunzoo@ya.ru>
2021-05-02 18:31:18 +0600

    new ver

--- win_timer.py
+++ win_timer.py
@@ -1,6 +1,8 @@
 import os
 print("--------------------------\nTERMINAL VERSION 0.1\n--------------------------\nProgram: shutdown computer\nAuthor: A.D.")
-def shut():
+print("\nChoose language (ru/en)")
+lang=input()
+def shut_en():
     print("\nВыключить или отменить выключение? 1/2")
     oper= input()
     if oper == "1":
@@ -13,7 +15,7 @@ def shut():
         if str(answer).upper() == ("N").upper:
             exit()
         elif str(answer).upper() == ("Y").upper():
-             shut()
+             shut_en()
         elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
             print("Неверный вариант ответа")
     elif oper == "2":
@@ -23,12 +25,47 @@ def shut():
         if str(answer).upper() == ("N").upper:
             exit()
         elif str(answer).upper() == ("Y").upper():
-            shut()
+            shut_en()
         elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
             print("Неверный вариант ответа")
     elif str(oper) != "1" or str(oper) != "2":
         print("Неверно введена операция")
-        shut()
-shut()
+        shut_en()
 
+def shut_ru():
+    print("\nВыключить или отменить выключение? 1/2")
+    oper= input()
+    if oper == "1":
+        print("Через сколько выключить компьютер?(минуты)")
+        time = input()
+        time_min = int(time) * 60
+        os.system("shutdown /s /t "+str(time_min))
+        print("Выполнить еще одну операцию? Y/N")
+        answer=input()
+        if str(answer).upper() == ("N").upper:
+            exit()
+        elif str(answer).upper() == ("Y").upper():
+             shut_ru()
+        elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
+            print("Неверный вариант ответа")
+    elif oper == "2":
+        os.system("shutdown /a")
+        print("Выполнить еще одну операцию? Y/N")
+        answer = input()
+        if str(answer).upper() == ("N").upper:
+            exit()
+        elif str(answer).upper() == ("Y").upper():
+            shut_ru()
+        elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
+            print("Неверный вариант ответа")
+    elif str(oper) != "1" or str(oper) != "2":
+        print("Неверно введена операция")
+        shut_ru()
+
+if lang.upper() ==  ("ru").upper():
+    shut_ru()
+elif lang.upper() == ("en").upper():
+    shut_en()
+elif lang.upper()!=("ru") or lang.upper()!= ("en"):
+    print("Invalid value selected")
 
