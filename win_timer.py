4dbb7613e75f386bc535fea74d7d882c48722c12
LeGunZoo <legunzoo@ya.ru>
2021-05-02 18:37:05 +0600

    new ver_3

--- win_timer.py
+++ win_timer.py
@@ -1,35 +1,35 @@
 import os
-print("--------------------------\nTERMINAL VERSION 0.1\n--------------------------\nProgram: shutdown computer\nAuthor: A.D.")
+print("--------------------------\nTERMINAL VERSION 0.3\n--------------------------\nProgram: shutdown computer\nAuthor: A.D.")
 print("\nChoose language (ru/en)")
 lang=input()
 def shut_en():
-    print("\nВыключить или отменить выключение? 1/2")
+    print("\nTurn off or cancel shutdown? 1/2")
     oper= input()
     if oper == "1":
-        print("Через сколько выключить компьютер?(минуты)")
+        print("How long does it take to turn off the computer?(minutes)")
         time = input()
         time_min = int(time) * 60
         os.system("shutdown /s /t "+str(time_min))
-        print("Выполнить еще одну операцию? Y/N")
+        print("Do one more operation? Y/N")
         answer=input()
         if str(answer).upper() == ("N").upper:
             exit()
         elif str(answer).upper() == ("Y").upper():
              shut_en()
         elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
-            print("Неверный вариант ответа")
+            print("Wrong answer option")
     elif oper == "2":
         os.system("shutdown /a")
-        print("Выполнить еще одну операцию? Y/N")
+        print("Do one more operation? Y/N")
         answer = input()
         if str(answer).upper() == ("N").upper:
             exit()
         elif str(answer).upper() == ("Y").upper():
             shut_en()
         elif str(answer).upper() != ("N").upper() or str(answer).upper() != ("Y").upper():
-            print("Неверный вариант ответа")
+            print("Wrong answer option")
     elif str(oper) != "1" or str(oper) != "2":
-        print("Неверно введена операция")
+        print("Operation entered incorrectly")
         shut_en()
 
 def shut_ru():
